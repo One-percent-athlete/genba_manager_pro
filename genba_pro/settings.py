24c8ae689dd9749ce9f161b261595e85b0dd5aca
One-percent-athlete <one.percent.athlete@gmail.com>
2024-12-13 22:08:51 +0900

    Setup railway database

--- genba_pro/settings.py
+++ genba_pro/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'genba',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,14 @@ WSGI_APPLICATION = 'genba_pro.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'railway',
+        'USER':'postgres',
+        'PASSWORD':'UfnzeastoFkIelyFGVjetzkDHGKqNckp',
+        'HOST':'autorack.proxy.rlwy.net',
+        'PORT':'24310',
     }
 }
 
