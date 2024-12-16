1777fa6a55b6a6ebc025127e57831453071ed0b3
One-percent-athlete <one.percent.athlete@gmail.com>
2024-12-16 20:01:46 +0900

    Bug fix on report list html

--- genba/views.py
+++ genba/views.py
@@ -270,10 +270,27 @@ def report_list(request):
 @login_required(login_url='/login_user/')
 def export_searched(request, keyword):
     if request.user.is_authenticated:
-        if request.method == "POST":
-            keyword = request.POST['keyword']
-            result_list = DailyReport.objects.filter(date_created__contains=keyword).order_by('-date_created')
-            return render(request, "report_search_list.html", {"result_list": result_list, "keyword": keyword})
+        result_list = DailyReport.objects.filter(date_created__contains=keyword).order_by('-date_created')
+        f = "現場毎作業日報" + "_" + ".csv"
+        response = HttpResponse(content_type='text/csv; charset=utf-8-sig')
+        filename = urllib.parse.quote((f).encode('utf-8'))
+        response['Content-Disposition'] = f'attachment; filename*=UTF-8\'\'{filename}'
+        writer = csv.writer(response)
+        if result_list:
+            writer.writerow(["作成日", "作業日", "作成者", "現場名", "取引先", "建退共", "作業員","人数", "シフト", "場所", "開始時間", "終了時間", "休憩時間", "高速道路乗り", "高速道路降り", "高速支払い方法", "駐車場料金", "宿泊料金", "その他支払い", "建替人", "作業内容", "連絡事項"])
+            for report in result_list:
+                attendees = []
+                for a in report.workers.all():
+                    attendees.append(a.fullname)
+                if report.kentaikyo:
+                    writer.writerow([report.date_created.date(), report.working_date, report.created_by, report.genba.name, report.genba.client, "有", attendees, attendees, report.shift, report.genba.address, report.start_time, report.end_time, report.break_time, report.highway_start, report.highway_end, report.highway_payment, report.parking, report.hotel, f"{report.other_payment, report.other_payment_amount}", report.paid_by, report.daily_details, report.daily_note ])
+                else:
+                    writer.writerow([report.date_created.date(), report.working_date, report.created_by, report.genba.name, report.genba.client, "無", attendees, attendees, report.shift, report.genba.address, report.start_time, report.end_time, report.break_time, report.highway_start, report.highway_end, report.highway_payment, report.parking, report.hotel, f"{report.other_payment, report.other_payment_amount}", report.paid_by, report.daily_details, report.daily_note ])
+                return response
+        else:
+            messages.success(request, "データがありません。")
+            return redirect("report_list")
+        return render(request, "report_search_list.html", {"result_list": result_list, "keyword": keyword})
 
 @login_required(login_url='/login_user/')
 def add_report(request):
@@ -283,10 +300,10 @@ def add_report(request):
         if form.is_valid():
             form.save()
             messages.success(request, ("作業日報を追加しました。"))
-            return redirect("report_search_list.html")
+            return redirect("report_list")
         else:
             messages.success(request, ("再度お試しください。"))
-            return redirect("report_search_list.html")
+            return redirect("report_list")
     else:
         return render(request, "add_report.html", {
             "form": form
