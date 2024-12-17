f718a3b9d415b87a7f727e0c0ca69413ab5c5e06
One-percent-athlete <one.percent.athlete@gmail.com>
2024-12-17 22:22:14 +0900

    Update register user form

--- genba/forms.py
+++ genba/forms.py
@@ -11,20 +11,20 @@ class SignUpForm(UserCreationForm):
 	def __init__(self, *args, **kwargs):
 		super(SignUpForm, self).__init__(*args, **kwargs)
 
-		self.fields['username'].widget.attrs['class'] = 'form-control'
+		self.fields['username'].widget.attrs['class'] = 'form-control p-2 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300'
 		self.fields['username'].widget.attrs['placeholder'] = 'ユーザー名'
 		self.fields['username'].label = ''
-		self.fields['username'].help_text = '<span class="form-text text-muted"><small>記号、スペースなし、150文字以下</small></span>'
+		self.fields['username'].help_text = '<ul class="form-text text-muted small"><li>記号、スペースなし</li><li>150文字以下</li></ul>'
 
-		self.fields['password1'].widget.attrs['class'] = 'form-control'
+		self.fields['password1'].widget.attrs['class'] = 'form-control p-2 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300'
 		self.fields['password1'].widget.attrs['placeholder'] = 'パスワード'
 		self.fields['password1'].label = ''
 		self.fields['password1'].help_text = '<ul class="form-text text-muted small"><li>ユーザー名と一致しない</li><li>8文字以上</li><li>数字のみ不可</li></ul>'
 
-		self.fields['password2'].widget.attrs['class'] = 'form-control'
+		self.fields['password2'].widget.attrs['class'] = 'form-control p-2 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300'
 		self.fields['password2'].widget.attrs['placeholder'] = '確認パスワード'
 		self.fields['password2'].label = ''
-		self.fields['password2'].help_text = '<span class="form-text text-muted"><small>再度パスワード入力</small></span>'
+		self.fields['password2'].help_text = '<ul class="form-text text-muted small"><li>再度パスワード入力</li></ul>'
     
 class UserProfileForm(forms.ModelForm):
 	CHOICE = [
